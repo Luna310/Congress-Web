c89d75d3d162d2f1216d7f4f7905175a3f440eac
Jorge Luna <jorge1980_1@hotmail.com>
2018-11-21 10:30:43 +0100

    terminado el filtrado de tablas

--- main.js
+++ main.js
@@ -1,12 +1,43 @@
 var members = data.results[0].members;
-console.log(members);
 
+
+function setState() {
+    state1 = ["all"];
+    for (i = 0; i < members.length; i++) {
+        if (state1.indexOf(members[i].state) === -1) {
+            state1.push(members[i].state);
+        }
+
+    }
+}
+
+
+
+
+function createDropdow() {
+
+    setState();
+    var newSelect = document.createElement("select");
+    newSelect.setAttribute("id", "selectState");
+    newSelect.setAttribute("name", "selectName")
+    newSelect.onchange = function () {
+        filterCongress();
+    };
+    document.getElementById("divDropdown").appendChild(newSelect);
+
+    for (i = 0; i < state1.length; i++) {
+        var optionState = document.createElement("option");
+        newSelect.appendChild(optionState);
+        optionState.textContent = state1[i];
+        optionState.setAttribute("value", state1[i]);
+        optionState.setAttribute("name", "estado")
+    }
+}
 var arrayThead = ["Full Name", "Party", "State", "Seniority", "Percentage of votes with party"];
-var num = 1;
 
-
-function addMultTable(rows, cols) {
+function addMultTable(rows, array) {
     var newTabla = document.createElement("table");
+    newTabla.classList.add("table", "table-striped");
     newTabla.setAttribute("id", "tabla1");
     document.getElementById("divTable").appendChild(newTabla);
     var tblHead = document.createElement("thead");
@@ -19,9 +50,9 @@ function addMultTable(rows, cols) {
     newTabla.appendChild(tblBody);
 
     function getName(i) {
-        var firstName = members[i].first_name;
-        var middleName = members[i].middle_name;
-        var lastName = members[i].last_name;
+        var firstName = array[i].first_name;
+        var middleName = array[i].middle_name;
+        var lastName = array[i].last_name;
 
         if (middleName == null) {
             middleName = " ";
@@ -39,36 +70,87 @@ function addMultTable(rows, cols) {
     }
 
 
-    for (var i = 0; i < members.length; i++) {
+    for (var i = 0; i < array.length; i++) {
         var newTr = document.createElement("tr");
         tblBody.appendChild(newTr);
-        
+
 
         var fullName = document.createElement("td");
         newTr.appendChild(fullName);
         var tagA = document.createElement("a");
         fullName.appendChild(tagA);
-        tagA.setAttribute("href", members[i].url);
+        tagA.setAttribute("href", array[i].url);
         tagA.textContent = getName(i);
 
         var party = document.createElement("td");
         newTr.appendChild(party);
-        party.textContent = members[i].party;
+        party.textContent = array[i].party;
 
         var state = document.createElement("td");
         newTr.appendChild(state);
-        state.textContent = members[i].state;
+        state.textContent = array[i].state;
 
         var seniority = document.createElement("td");
         newTr.appendChild(seniority);
-        seniority.textContent = members[i].seniority;
+        seniority.textContent = array[i].seniority;
 
         var votes = document.createElement("td");
         newTr.appendChild(votes);
-        votes.textContent = members[i].votes_with_party_pct + " %";
+        votes.textContent = array[i].votes_with_party_pct + " %";
 
     }
 
     console.log(newTabla);
 }
-addMultTable(arrayThead.length, members.length);
+addMultTable(arrayThead.length, members);
+
+function filterCongress() {
+
+    var dropTable = document.getElementById("divTable");
+    dropTable.innerHTML = "";
+
+    var stateFinal = [];
+
+    var accesState = document.getElementById("selectState").value;
+    console.log(accesState)
+    if (accesState == "all") {
+        console.log(stateFinal)
+        stateFinal = members;
+
+    } else {
+        for (j = 0; j < members.length; j++) {
+            if (accesState == members[j].state) {
+                stateFinal.push(members[j]);
+            }
+        }
+    }
+
+    var checkBox = document.querySelectorAll('input[name=party]:checked');
+    var membersFilter = [];
+    for (i = 0; i < checkBox.length; i++) {
+        for (j = 0; j < members.length; j++) {
+            if (checkBox[i].value == members[j].party) {
+                membersFilter.push(members[j]);
+            }
+        }
+    }
+    var multiFilter = [];
+    console.log(stateFinal, "statefinal");
+    for (i = 0; i < membersFilter.length; i++) {
+        for (j = 0; j < stateFinal.length; j++){
+            console.log( stateFinal[j])
+            if (membersFilter[i] == stateFinal[j]) {
+                multiFilter.push(membersFilter[i])
+
+            }
+    }
+    }
+    console.log(multiFilter, "multifilter");
+
+        addMultTable(arrayThead.length, multiFilter);
+
+
+}
+
+addMultTable(arrayThead.length, members);
+createDropdow();
